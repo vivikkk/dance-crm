defef8d2dfd4b693dfebf8c2850351787ba3fd97
Ivan <ivanushka.frolov@gmail.com>
2020-03-31 12:57:05 +0300

    login validate

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,13 +15,18 @@ v-container(
         )
           v-toolbar-title Форма входа
         v-card-text
-          v-form
+          v-form(
+            v-model="valid"
+            ref="form"
+            lazy-validation
+          )
             v-text-field(
               label="Почта"
               name="email"
               v-model.trim="email"
               prepend-icon="mdi-email"
               type="email"
+              :rules="emailRules"
             )
             v-text-field(
               label="Пароль"
@@ -29,40 +34,52 @@ v-container(
               v-model.trim="password"
               prepend-icon="mdi-lock"
               type="password"
+              :rules="passwordRules"
             )
         v-card-actions
           v-spacer
           v-btn(
             text color="primary"
             :loading="loading"
-            :disabled="loading"
+            :disabled="!valid"
             @click="onSubmit"
           ) Войти
 </template>
 
 <script>
+const emailRegex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(\.\w{2,3})+$/
+
 export default {
   name: 'Login',
 
   data () {
     return {
       loading: false,
-      valid: false,
       email: null,
-      password: null
+      password: null,
+      valid: false,
+      emailRules: [
+        v => !!v || 'Обязательное поле',
+        v => emailRegex.test(v) || 'Невалидная почта'
+      ],
+      passwordRules: [
+        v => !!v || 'Обязательное поле'
+      ]
     }
   },
 
   methods: {
     async onSubmit () {
-      const loginForm = {
-        email: this.email,
-        password: this.password
-      }
-      this.loading = true
+      if (this.$refs.form.validate()) {
+        const loginForm = {
+          email: this.email,
+          password: this.password
+        }
 
-      await this.$store.dispatch('login', loginForm)
-      this.$router.push('/')
+        this.loading = true
+        await this.$store.dispatch('login', loginForm)
+        this.$router.push('/')
+      }
     }
   }
 }
